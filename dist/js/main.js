be3e792e9ebbb0830c48a3b9b6909ab67211cbcc
DannyMcMillan <mcmillandanny@gmail.com>
2018-05-24 18:38:12 -0400

    updated styles

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,4 +1,15 @@
-"use strict";
+'use strict';
 
 console.log("Webdev-Final");
+
+var btn = document.querySelector('.btn');
+
+var dropDown = document.querySelector('.text-editors-menu');
+console.log(dropDown);
+
+btn.addEventListener('click', function () {
+	dropDown.classList.toggle('closed');
+
+	console.log("fick");
+});
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,15 @@
 console.log("Webdev-Final");
+
+
+
+let btn = document.querySelector('.btn');
+
+let dropDown = document.querySelector('.text-editors-menu');
+console.log(dropDown)
+
+btn.addEventListener('click', function() {
+	dropDown.classList.toggle('closed');
+
+	console.log("fick");
+})
+	
